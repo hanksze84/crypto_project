3bbe0693ef2946cb103de951e287d1f30418e1fa
Hank <hanksze84@gmail.com>
2019-08-04 16:01:39 -0700

    see-price/ticker

---
+++ app.js
@@ -0,0 +1,21 @@
+const tickersContainer = document.getElementById('tickers');
+
+
+fetch('https://api.coingecko.com/api/v3/exchanges/gdax').then(function(res){
+  return res.json();
+}).then(function(res) {
+  const tickers = res.tickers;
+  console.log('res', res);
+  let htmlStr = '';
+
+  for (let i = 0; i <= tickers.length - 1; i+=1)  {
+      const currentTicker = tickers[i]; 
+      const base = currentTicker.base;
+      const target = currentTicker.target; 
+      const price = currentTicker.last; 
+    htmlStr = htmlStr + `<div>${base}/${target}<strong>${price}</strong></div>`; 
+  }
+
+  console.log('htmlStr', htmlStr);
+  tickersContainer.innerHTML = htmlStr;
+});
